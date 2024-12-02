c627f390ff6d5d3ef54b7658567b14d5b7d1f5b7
mbhashkar <113530747+little-fox14@users.noreply.github.com>
2024-12-02 22:32:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,3 @@
-// const express = require("express");
-// const dotenv = require("dotenv");
-// const fileUpload = require("express-fileupload");
-// const dbConnection = require("./Database/dbConnection.js");
-
-// const assignmentRoutes = require("./routes/assignmentRoutes.js");
-// // Load environment variables
-// dotenv.config();
-
-// const app = express();
-
-// // Middleware
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(fileUpload());
-
-// // Connect to the database
-// dbConnection();
-
-// //routes
-
-// app.use("/api/assignments", assignmentRoutes);
-// // Define the port
-// const port = process.env.PORT || 5000; // Default to 5000 if PORT is undefined
-
-// // Start the server
-// app.listen(port, () => {
-//     console.log(`Server running on port ${port}`);
-// });
-
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
